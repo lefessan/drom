ffffd8825b0f7a0a1229a0b7842ae4b51811095c
lefessan <fabrice.le_fessant@ocamlpro.com>
2022-08-10 10:16:25 +0000

    Deploying to gh-pages from  @ 8ca686192089bd50f03046705e615b8cb4236876 🚀

--- sphinx/_static/documentation_options.js
+++ sphinx/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'master (bb9c4b6) (2021/11/23 18:11)',
+    VERSION: 'master (8ca6861) (2022/08/10 10:16)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
